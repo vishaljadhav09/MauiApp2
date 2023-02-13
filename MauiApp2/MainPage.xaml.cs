c21dc1c3453d41bd494da93ef52907d036a6d675
Vishal Jadhav <vjadhav@qbacorp.com>
2023-02-13 10:39:51 +0530

    Adding Files

---
+++ MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void OnBlueBackClicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void OnLocationSwitchToggled(object sender, ToggledEventArgs e)
+    {
+
+    }
+
+    private void AttachmentClicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
